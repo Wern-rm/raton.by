678f1a849c715b960ebb0960b2b51d6a770d0c56
WeRn <devwern@gmail.com>
2021-04-27 11:25:26 +0300

    Контакты

---
+++ app/forms/main_contacts.py
@@ -0,0 +1,23 @@
+from wtforms import *
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length, Email, Required, EqualTo
+
+
+class CreateMessageForm(FlaskForm):
+    """Форма добавления обратной связи"""
+    name = StringField('Ваше имя', validators=[
+        DataRequired(message='Обязательное поле'),
+        Length(min=2, max=15, message='Имя должно состоять не менее чем из 2 и не более 15 символов.')
+    ])
+    email = StringField('Ваш email', validators=[
+        Email(message='Обязательное поле'),
+        Length(min=6, max=30, message='Email должен быть не менее 2 и не более 30 символов.')
+    ])
+    phone = StringField('Ваш номер телефона', validators=[
+        DataRequired(message='Обязательное поле'),
+        Length(min=12, max=13, message='Номер телефона должнен состоять не менее чем из 12 и не более 13 символов.')
+    ])
+    text = TextAreaField('Ваш вопрос', validators=[
+        DataRequired(message='Обязательное поле'),
+        Length(min=1, max=1000, message='Вопрос должен состоять не более чем из 1000 символов.')
+    ])
--- app/views/main/contacts.py
+++ app/views/main/contacts.py
@@ -1,14 +1,32 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
-from app import db
+from app import db, logger
 from app.controllers.app_controller import app_controller
 from app.models.contacts import Contacts
 from app.views.main import bp
+from app.forms.main_contacts import CreateMessageForm
+from app.models.questions import Questions
 
 
-@bp.route('/contacts')
+@bp.route('/contacts', methods=['GET', 'POST'])
 @app_controller
 def contacts(**kwargs):
+    form = CreateMessageForm()
+    if form.validate_on_submit():
+        try:
+            new = Questions(username=form.name.data,
+                            email=form.email.data,
+                            phone=form.phone.data,
+                            message=form.text.data)
+            db.session.add(new)
+            db.session.commit()
+            return redirect(url_for('main.contacts', action='success', id=131))
+        except Exception as e:
+            db.session.rollback()
+            logger.error(e)
+            return redirect(url_for('main.contacts', action='error', id=13213))
+
     kwargs['title'] = 'Контакты'
     kwargs['contacts'] = db.session.query(Contacts).order_by(Contacts.id).all()
+    kwargs['form'] = form
     return render_template("default/contacts.html", **kwargs)
